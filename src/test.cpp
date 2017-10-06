bdfef5500c0f73786babe8c4df6b17d20844fe2f
Nejc Galof <galof.nejc@gmail.com>
2017-10-06 22:44:11 +0200

    first commit

---
+++ src/test.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <test.h>
+
+using namespace std;
+
+int main(void) {
+
+     cout << "Hello World" << endl;
+	 cout << sum(3, 5);
+     cin.get();
+     return(0);
+
+}
